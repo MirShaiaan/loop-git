eae2eb77ee3159cecb29febbdf138c9583eeaf27
Faizan <mirfaizan8803@gmail.com>
2025-07-25 16:01:45 +0530

    I added the while loop for range of prime. I also did some minor fixes in the for loop of Shaiaan. Time: 4:01pm Date: 25/07/2025.

--- day9/Program.cs
+++ day9/Program.cs
@@ -12,64 +12,76 @@ namespace day9
     {
         static void Main(string[] args)
         {
+            //// done by shaiaan -- for loop on 25/07/2025 at 3:30pm
+
+            //done some changes too
+
+            //Console.WriteLine(" the range of prime ");
+
+            //Console.WriteLine(" the minimum range of prime numbers");
+            //int min = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" the maximum range of prime numbers");
+            //int max = int.Parse(Console.ReadLine());
+
+
+            //for (int i = min; min <= max; min++)
+            //{
+            //    int flag = 0;               //0 means prime
+            //    for (int b = 2; b < min; b++)
+            //    {
+            //        if (min % b == 0)
+            //        {
+            //            flag = 1;
+            //        }
+
+
+            //    }
+            //    if (flag == 1)
+            //    {
+            //        Console.WriteLine(min + " COMPOSITE NUMBER.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(min + " PRIME NUMBER.");
+            //    }
+            //}
 
 
 
-            Console.WriteLine(" the range of prime ");
-
-            Console.WriteLine(" the minimum range of prime numbers");
-            int min = int.Parse(Console.ReadLine());
-            Console.WriteLine(" the maximum range of prime numbers");
-            int max = int.Parse(Console.ReadLine());
+            // range of prime done by faizan ----- 25/07/2025 at 3:54pm.
 
 
-            for (min = min; min <= max; min++)
+            Console.WriteLine("======Range of prime using while loop======");
+
+            Console.Write("Enter the minimum value: ");
+            int min2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the maximum value: ");
+            int max2 = int.Parse(Console.ReadLine());
+
+            while(min2 <= max2)
             {
-                int toy = 0;
-                int b = 2;
-                for (b = 2; b < min; b++)
+                int flag = 0;
+                int i = 2;
+                while(i < min2)
                 {
-                    if (min % b == 0)
+                    if(min2 % i == 0)
                     {
-                        toy = 1;
+                        flag = 1;
                     }
-
-
-
+                    i++;
                 }
-                if (toy == 1)
+
+                if(flag == 1)
                 {
-                    Console.WriteLine(min + " COMPOSITE NUMBER ");
+                    Console.WriteLine($"{min2} is composite!");
                 }
                 else
                 {
-                    Console.WriteLine(min + " PRIME NUMBER ");
+                    Console.WriteLine($"{min2} is prime!");
                 }
+                min2++;
             }
-
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
